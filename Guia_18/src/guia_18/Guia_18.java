447f7de6dd0f6233f22b8098255b3548ceabe9e4
FaberVillarreal18 <faber.villarrealantequera@unicolombo.edu.co>
2024-04-21 01:39:10 -0500

    Guia 18

---
+++ Guia_18/src/guia_18/Guia_18.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guia_18;
+
+import gui.Ventanas;
+
+/**
+ *
+ * @author rodri
+ */
+public class Guia_18 {
+     public static void main(String[] args){
+         Ventanas x = new Ventanas();
+         x.setVisible(true);
+     }
+}
